e8f4220f41118b5926d93df400de89f1402d4be9
Sampath7890 <gsksd64@gmail.com>
2025-03-10 22:56:08 +0530

    Add files via upload

---
+++ sam.cpp/DSAC++LEARING.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"hello world\n my name is G.Sampath" << endl;
+    int age = 19+1;
+    float height =5.11;
+    char Rank = 'A';
+ double weight =55.66;
+ cout << "age is " << age <<endl;
+ cout << "height is "<< height << endl;
+ cout << "rank is "<< Rank << endl;
+ cout << "weight is "<< weight << endl;
+return 0;
+}
