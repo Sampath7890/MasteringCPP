8affa02f9979905441af45586a6fda7075994e59
Sampath7890 <gsksd64@gmail.com>
2025-03-11 23:25:37 +0530

    Update c++input.cpp

--- sam.cpp/c++input.cpp
+++ sam.cpp/c++input.cpp
@@ -1,1 +1,13 @@
 
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a,b,c;
+    cout<<"Enter two values:";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum is"<<c<<endl;
+
+    return 0;
+}
