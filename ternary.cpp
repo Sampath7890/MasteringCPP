eedc646d611d928e50407355af4d068e715a94c9
Sampath7890 <gsksd64@gmail.com>
2025-03-13 19:56:27 +0530

    Create ternary.cpp

---
+++ ternary.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter a number:";
+    cin>>num;
+    cout<<(num>=0 ? "posittive" : "negative")<<endl;
+
+    return 0;
+} 
